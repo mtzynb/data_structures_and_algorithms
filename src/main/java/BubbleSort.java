c4ae981c4033c7994a9708f143bc0feda5aa3099
mtzynb <zeynab.mohammadtabar@gmail.com>
2022-01-13 22:04:46 +0330

    - add Generic to bubble sort!

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -5,36 +5,50 @@ import java.util.Arrays;
  * @created 13/01/2022
  */
 
-public class BubbleSort {
+public class BubbleSort<T extends Comparable<T>> {
     public static void main(String[] args) {
 
-        int[] arr = {20, 35, -15, 7, 55, 1, -22};
-        bubbleSort(arr);
-
+        // sort integers!
+        Integer[] arr = {20, 35, -15, 7, 55, 1, -22};
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+        bubbleSort.bubbleSort(arr);
         Arrays.stream(arr).forEach(System.out::println);
+
+        // sort strings!
+        String[] unsortedStringArr = {"Rahaa", "Zey", "Zeynab", "Zeinab"};
+        BubbleSort<String> stringBubbleSort = new BubbleSort<>();
+        stringBubbleSort.bubbleSort(unsortedStringArr);
+        Arrays.stream(unsortedStringArr).forEach(System.out::println);
+
+        // sort doubles!
+        Double[] unsorterdDoubleArr = {0.8, 2.5, 5.0, 100.0, 0.5, 0.1, -0.8};
+        BubbleSort<Double> doubleBubbleSort = new BubbleSort<>();
+        doubleBubbleSort.bubbleSort(unsorterdDoubleArr);
+        Arrays.stream(unsorterdDoubleArr).forEach(System.out::println);
+
     }
 
-    public static void swap(int[] arr, int i, int j) {
 
-        if (i == j) {
-            return;
-        }
-
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-
-    }
-
-    public static void bubbleSort(int[] arr) {
+    public void bubbleSort(T[] arr) {
 
         for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (arr[i] > arr[i + 1]) {
+                if (arr[i].compareTo(arr[i + 1]) > 0) {
                     swap(arr, i, i + 1);
                 }
             }
         }
+
+    }
+
+    private void swap(T[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        T tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
 
     }
 }
